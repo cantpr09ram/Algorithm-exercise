386cb9c58ba9c782a653a2ff5054e56bc70a7065
bochain <bochainwu@gmail.com>
2021-02-13 07:36:01 +0800

    finish insertion sort

--- Insertion_sort/instertion_sort.cpp
+++ Insertion_sort/instertion_sort.cpp
@@ -3,13 +3,26 @@ using namespace std;
 
 int main(){
 	int array[] = {1,3,5,7,9,2,4,0,8,6};
-	cout<<"before sort"
+	int temp ,next;
+	cout<<"before sort"<<endl;
 	for(int i; i < 10;i++){
 		cout<<array[i];
 	}
 	cout<<endl;
 	
 	for(int i; i < 10; i++){
-		
+		temp = array[i];
+		next = i;
+		while((next > 0 )&&(array[next-1] > temp)){
+			array[next] = array[next - 1];
+			next--;
+		}	
+	array[next] = temp;	
 	}
+
+	cout<<"after sort"<<endl;	
+	for(int i;i < 10;i++){
+		cout<<array[i];
+	}	
+	cout<<endl;
 }
