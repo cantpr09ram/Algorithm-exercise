4faa4d80dcf47a80f731f8a54b68c74abb25c9ca
bochain <bochainwu@gmail.com>
2021-02-12 21:53:42 +0800

    updata

--- Insertion_sort/instertion_sort.cpp
+++ Insertion_sort/instertion_sort.cpp
@@ -3,5 +3,13 @@ using namespace std;
 
 int main(){
 	int array[] = {1,3,5,7,9,2,4,0,8,6};
+	cout<<"before sort"
+	for(int i; i < 10;i++){
+		cout<<array[i];
+	}
+	cout<<endl;
 	
+	for(int i; i < 10; i++){
+		
+	}
 }
