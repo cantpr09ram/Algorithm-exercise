9987982d66dd3660652d9001b0c2dbbaa2bfa530
bochain <bochainwu@gmail.com>
2021-02-14 20:46:16 +0800

    frist done

--- quick_sort/quick_sort.cpp
+++ quick_sort/quick_sort.cpp
@@ -1,21 +1,20 @@
-// C++ code
-
 #include <iostream>
 using namespace std;
 
-int Partition(int *arr, int front, int end){
-    int pivot = arr[end];
+int Partition(int array[], int front, int end){
+    int pivot = array[end];
     int i = front -1;
     for (int j = front; j < end; j++) {
-        if (arr[j] < pivot) {
+        if (array[j] < pivot) {
             i++;
-            swap(&arr[i], &arr[j]);
+            swap(array[i], array[j]);
         }
     }
     i++;
-    swap(&arr[i], &arr[end]);
+    swap(array[i],array[end]);
     return i;
 }
+
 void QuickSort(int *arr, int front, int end){
     if (front < end) {
         int pivot = Partition(arr, front, end);
@@ -26,17 +25,17 @@ void QuickSort(int *arr, int front, int end){
 
 int main() {
 
-    int n = 9;
-    int array[] = {9, 4, 1, 6, 7, 3, 8, 2, 5};
+    int n = 10;
+    int array[] = {0,9,8,7,6,5,4,3,2,1};
     cout << "before sort"<<endl;
     for(int i; i<10;i++){
-    	cout<<"array[i]";
+    	cout<<array[i];
 	}
 	cout<<endl;
-    QuickSort(arr, 0, n-1);
+    QuickSort(array, 0, n-1);
 
     cout << "after sort"<<endl;
-    for(int i;i<10;i++){
+    for(int i = 0;i<10;i++){
     	cout<<array[i];
 	}
     return 0;
---
+++ quick_sort/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a=1,b = 2;
+    cout<<a<<b<<endl;
+    swap(a,b);
+    cout<<a<<b<<endl;
+    return 0;
+}
