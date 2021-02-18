e8d1464e7c2214ff8090921e03cf9573b98f85f4
bochain <bochainwu@gmail.com>
2021-02-18 22:35:00 +0800

    updata bogo sort

--- bogo_sort/bogo_sort.cpp
+++ bogo_sort/bogo_sort.cpp
@@ -6,24 +6,31 @@ int main()
   std::random_device rd;
   std::default_random_engine gen = std::default_random_engine(rd());
   std::uniform_int_distribution<int> dis(1,10);
-
-  int array[] = {1,2,3,4,5,6,7,8,9,0};
-  int numbers[10];
-  for (int i=0; i<10; ++i){
+  int n = 10;
+  int array[] = {0,9,8,7,6,5,4,3,2,1};
+  
+  while(1){
+    int numbers[10];
+    int answers[10];
+    for (int i=0; i<10; ++i){
       numbers[i] = dis(gen);
-      for(int j=0;j<i;j++)
-        {
-             if(numbers[i] == numbers[j])
-             {
+      for(int j=0;j<i;j++){
+        if(numbers[i] == numbers[j]){
                  i--;
                  break;
-             }
         }
-  }
-  while(1){
-    for(int i; i<10; i++){
-      
+      }
     }
+    for(int i; i<10; i++){
+      answers[i] = array[numbers[i]];
+    }
+
+    while(--n > 1){
+      if(answers[n] < answers[n - 1]){
+        break;
+      }
+    }
+    
   }
   return 0;
 }
