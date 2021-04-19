ca575681be5c688b96a220c2ade31df59ee20ee5
bo_chain <53370227+bochainwu@users.noreply.github.com>
2021-04-19 14:53:45 +0800

    updata linked_list

--- linked_list/linked_list.h
+++ linked_list/linked_list.h
@@ -1,3 +1,6 @@
+//linked_list
+#include <stdio.h>
+#include <stdlib.h> 
 struct node
 {
     int data;
@@ -6,9 +9,52 @@ struct node
 
 typedef struct node NODE;
 
-NODE *createList(int *, int);
-void printList(NODE *);
-void freeList(NODE *);
+//NODE *createList(int *, int);
+//void printList(NODE *);
+//void freeList(NODE *);
 void insertList(NODE *);
 void *searchList(NODE *);
 void *deleteList(NODE *);
+
+/*creataList*/
+NODE *createList(int *arr,int len){
+    int i;
+    NODE *frist, *current, *previous;
+    for( i; i<len; i++){
+        current = (NODE*) malloc(sizeof(NODE));
+        current -> data = arr[i];
+        if(i==0)
+            frist = current;
+        else
+            previous -> next = current;
+        current -> next = NULL;
+        previous = current;
+    }
+    return frist;
+}
+
+/*printList*/
+void printList(NODE *frist){
+    NODE* node = frist;
+    if(frist = NULL)
+        printf("List is empty!");
+    else{
+        while(node!=NULL){
+            printf("%3d", node->data);
+            node = node ->next;
+        }
+        printf("\n");
+    }
+}
+
+/*freeList*/
+void freeList(NODE *frist){
+    NODE *current, *tmp;
+    current = frist;
+    while (current != NULL)
+    {
+        tmp = current;
+        current = current -> next;
+        free(tmp);
+    }   
+}
--- linked_list/use_linked_list.c
+++ linked_list/use_linked_list.c
@@ -8,50 +8,6 @@ int main(void){
     frist = createList(arr,3);
     printList(frist);
     freeList(frist);
-
     system("pause");
     return 0;
-}
-
-/*creataList*/
-NODE *createList(int *arr,int len){
-    int i;
-    NODE *frist, *current, *previous;
-    for(int i;i<len;i++){
-        current = (NODE*) malloc(sizeof(NODE));
-        current -> data = arr[i];
-        if(i==0)
-            frist = current;
-        else
-            previous -> next = current;
-        current -> next = NULL;
-        previous = current;
-    }
-    return frist;
-}
-
-/*printList*/
-void printList(NODE *frist){
-    NODE* node = frist;
-    if(frist = NULL)
-        printf("List is empty!");
-    else{
-        while(node!=NULL){
-            printf("%3d", node->data);
-            node = node ->next;
-        }
-        printf("\n");
-    }
-}
-
-/*freeList*/
-void freeList(NODE *frist){
-    NODE *current, *tmp;
-    current = frist;
-    while (current != NULL)
-    {
-        tmp = current;
-        current = current -> next;
-        free(tmp);
-    }   
 }
